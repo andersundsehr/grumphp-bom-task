fb88c70d1e7f55eb644ef9d9aff6659a3521db58
Matthias Vogel <m.vogel@andersundsehr.com>
2019-10-29 08:35:53 +0000

    :arrow_up: upgrade grumphp to 0.16.* and php to mininum 7.2

--- src/ExtensionLoader.php
+++ src/ExtensionLoader.php
@@ -1,28 +1,13 @@
 <?php
+
 namespace PLUS\GrumPHPBomTask;
 
 use GrumPHP\Extension\ExtensionInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
 
-/**
- * Class ExtensionLoader
- *
- * @author Matthias Vogel <matthias.vogel@pluswerk.ag>
- * @package PLUS\GrumphpBomTask
- */
 class ExtensionLoader implements ExtensionInterface
 {
-    /**
-     * @param ContainerBuilder $container
-     *
-     * @return \Symfony\Component\DependencyInjection\Definition
-     * @throws \Exception
-     * @throws \Symfony\Component\DependencyInjection\Exception\BadMethodCallException
-     * @throws \Symfony\Component\DependencyInjection\Exception\InvalidArgumentException
-     * @throws \Symfony\Component\DependencyInjection\Exception\ServiceCircularReferenceException
-     * @throws \Symfony\Component\DependencyInjection\Exception\ServiceNotFoundException
-     */
     public function load(ContainerBuilder $container)
     {
         return $container->register('task.plus_bom_fixer', BomFixerTask::class)
